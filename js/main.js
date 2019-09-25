29623dadc802da01b8ec5d0e098d1e3c478fb947
Ari <gryphon150@gmail.com>
2019-09-25 16:47:37 -0700

    finished basic layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 console.log("👀")
+
+
+fetch("https://api.kanye.rest/")
+    .then(function(randomQuote){
+        return randomQuote.json()
+    })
+    .then(function(jsonQuote){
+        let quote = document.getElementById("quote")
+        // console.log(jsonQuote.quote)
+        quote.textContent = ` "${jsonQuote.quote}"`
+    
+        });
